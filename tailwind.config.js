97aec4cb06cfe8c99068c238cc11c54fa5921c3c
Rishi Mohan <iamrishi.ms@gmail.com>
2021-11-03 14:42:26 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    "./pages/*.js"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
